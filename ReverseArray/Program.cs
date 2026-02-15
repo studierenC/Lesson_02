6e3596393a1c1b16314a1736a244e1777d59bea7
Valery Semenenko <gearmobile@gmail.com>
2026-02-15 15:59:50 +0300

    feat: reverse array

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ReverseArray;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine($"Element {i}: {arr[i]}");   
+        }
+    }
+}
