71902496b1c5c9c3e0129630510605b61a4c415e
Valery Semenenko <gearmobile@gmail.com>
2026-02-15 15:45:54 +0300

    array from keyboard

---
+++ ArrayFromKeyboard/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ArrayFromKeyboard;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("Введите любое целое число: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Массив, который получился в итоге.");
+        foreach (var i in arr)
+        {
+            Console.WriteLine($"Элемент массива: {i}\t");
+        }
+    }
+}
