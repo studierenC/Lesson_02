5aaa7bd923b0713b5ffcde123c06cd105b1da67c
Valery Semenenko <gearmobile@gmail.com>
2026-02-15 16:04:09 +0300

    feat: array even elements sum

--- ReverseArray/Program.cs
+++ ReverseArray/Program.cs
@@ -4,11 +4,17 @@ class Program
 {
     static void Main()
     {
+        int evenSum = 0;
         int[] arr = { 1, 2, 3, 4, 5 };
 
         for (int i = arr.Length - 1; i >= 0; i--)
         {
-            Console.WriteLine($"Element {i}: {arr[i]}");   
+            Console.WriteLine($"Element {i}: {arr[i]}");
+            if (arr[i] % 2 == 0)
+            {
+                evenSum += arr[i];
+            }
         }
+        Console.WriteLine($"Сумма четных элементов в массиве равна: {evenSum}");
     }
 }
